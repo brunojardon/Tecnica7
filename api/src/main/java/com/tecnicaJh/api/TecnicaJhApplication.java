1569d54dc41637d1a70ff70258e9a5320f890d56
Bruno Jardon <brunojardon3@gmail.com>
2023-10-28 16:02:18 -0300

    Archivos base de la API

---
+++ api/src/main/java/com/tecnicaJh/api/TecnicaJhApplication.java
@@ -0,0 +1,13 @@
+package com.tecnicaJh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicaJhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicaJhApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/tecnicaJh/api/TecnicaJhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnicaJh.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicaJhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
